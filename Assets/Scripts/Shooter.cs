6bc88e2552f4913c9f39c8db4afeaed3f7207ff3
JFresnedaFuentespina <jfresnedafuentespina@gmail.com>
2026-04-22 11:46:04 +0200

    particulas disparo

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,13 +4,17 @@ public class Shooter : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject playerCamera;
+    public GameObject muzzleFlash;
+    private ParticleSystem muzzleFlashParticleSystem;
     private AudioSource audioSource;
     public AudioClip shootSound;
+    public Animator animator;
     public float range = 5f;
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         audioSource.clip = shootSound;
+        muzzleFlashParticleSystem = muzzleFlash.GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
@@ -25,6 +29,11 @@ public class Shooter : MonoBehaviour
     void Shoot()
     {
         audioSource.Play();
+        animator.SetTrigger("Shoot");
+        
+        muzzleFlashParticleSystem.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        muzzleFlashParticleSystem.Play();
+
         RaycastHit hit;
         if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, range))
         {
